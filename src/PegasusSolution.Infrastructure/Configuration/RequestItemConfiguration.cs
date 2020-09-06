791c51d15acf0b74f453ecc86532a0cf55368d57
Lester Lucas <lesterlucasit@hotmail.com>
2020-09-06 16:51:41 -0300

    fet(#2): implemented Context class

---
+++ src/PegasusSolution.Infrastructure/Configuration/RequestItemConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PegasusSolution.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PegasusSolution.Infrastructure.Configuration
+{
+    public class RequestItemConfiguration : IEntityTypeConfiguration<RequestItem>
+    {
+        public void Configure(EntityTypeBuilder<RequestItem> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PegasusSolution.Infrastructure/Context/Context1/PegasusDbContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PegasusSolution.Domain.Entities;
+using PegasusSolution.Domain.ValueObject.Payments;
+using PegasusSolution.Infrastructure.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PegasusSolution.Infrastructure.Context.Context1
+{
+    public class PegasusDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<RequestItem> RequestItems { get; set; }
+        public DbSet<PaymentMethod> PaymentMethod { get; set; }
+
+        public PegasusDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Mapping classes
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new RequestConfiguration());
+            modelBuilder.ApplyConfiguration(new RequestItemConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new PaymentMethodConfiguration());
+
+            modelBuilder.Entity<PaymentMethod>().HasData(
+                new PaymentMethod()
+                {
+                    Id = 1,
+                    Name = "Bankslip",
+                    Description = "Payment Method Bankslip"
+                },
+
+                new PaymentMethod()
+                {
+                    Id = 2,
+                    Name = "Credit Card",
+                    Description = "Payment Method Credit Card"
+                }
+                );
+
+            base.OnModelCreating(modelBuilder);
+
+
+
+
+
+        }
+
+    }
+}
