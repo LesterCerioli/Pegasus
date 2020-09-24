89eb58468b89a9ef09709d548828d8b90d57d4ce
LesterCerioli <lesterlucasit@hotmail.com>
2020-09-24 02:45:17 +0000

    implementing Interfaces and Entities

---
+++ src/1-Pegasus-API/PegasusSolution.Domain/Entities/Request.cs
@@ -0,0 +1,50 @@
+﻿using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PegasusSolution.Domain.Entities
+{
+    
+    
+    public class Request : Entity, IAggregateRoot
+    {
+        public Request(Guid id, DateTime requestDate, DateTime deliveryPrevisiousDate, string postalCode, string state, string city, string fullAddress, string district, string country, int number)
+        {
+            Id = id;
+            Number = number;
+            RequestDate = requestDate;
+            DeliveryPrevisiousDate = deliveryPrevisiousDate;
+            PostalCode = postalCode;
+            State = state;
+            City = city;
+            FullAddress = fullAddress;
+            District = district;
+            Country = country;
+        }
+        protected Request() { }
+        public int Number { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime DeliveryPrevisiousDate { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string FullAddress { get; set; }
+        public string District { get; set; }
+        public string Country { get; set; }
+
+    }
+    
+
+    
+
+
+
+
+
+
+
+}
+    
+    
+    
---
+++ src/1-Pegasus-API/PegasusSolution.Domain/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using NetDevPack.Domain;
+using System;
+
+
+namespace PegasusSolution.Domain.Entities
+{
+    public class User : Entity, IAggregateRoot
+    {
+        public User(Guid id, string email, string password, string name, string lastName, bool administrator)
+        {
+            Id = id;
+            Email = email;
+            Password = password;
+            Name = name;
+            LastName = lastName;
+            Administrator = administrator;
+        }
+
+        protected User() { }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool Administrator { get; set; }
+    }
+}
